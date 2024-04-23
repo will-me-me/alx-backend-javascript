e762b117bce74d840703749ac520afca02c58419
will-me-me <willykaria777@gmail.com>
2024-04-23 03:38:29 -0700

    Handle multiple successful promises

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,8 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  const promises = Promise.all([uploadPhoto(), createUser()]);
+  return promises
+    .then((res) => console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`))
+    .catch(() => console.log('Signup system offline'));
+}
