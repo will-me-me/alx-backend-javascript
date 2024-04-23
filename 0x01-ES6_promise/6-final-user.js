c5641221345959d4a9865bda5ec0f98c6682abaa
will-me-me <willykaria777@gmail.com>
2024-04-23 03:44:43 -0700

    Handle multiple promises

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,10 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const promise = Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
+  return promise.then((res) => res.map((obj) => ({
+    status: obj.status,
+    value: obj.status === 'fulfilled' ? obj.value : String(obj.reason),
+  })));
+}
