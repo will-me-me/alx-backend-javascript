3e9b4bb935803111458493d936bc57509b572dad
will-me-me <willykaria777@gmail.com>
2024-04-25 02:26:19 -0700

    Primitive - Holberton Class

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,24 @@
+/* eslint-disable */
+
+export default class HolbertonClass {
+  constructor(size, location) {
+    this._size = size;
+    this._location = location;
+  }
+
+  get size() {
+    return this._size;
+  }
+
+  valueOf() {
+    return this._size;
+  }
+
+  get location() {
+    return this._location;
+  }
+
+  toString() {
+    return this._location;
+  }
+}
