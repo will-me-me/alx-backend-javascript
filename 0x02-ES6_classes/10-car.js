f1efa1b020fc85e94e8a98bc221b6e7995b8a1c6
will-me-me <willykaria777@gmail.com>
2024-04-25 02:31:50 -0700

    Implement a class named Car:
    Constructor attributes:
    brand (String)
    motor (String)
    color (String)

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,30 @@
+/* eslint-disable */
+
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  static get [Symbol.species]() {
+    return this;
+  }
+
+  cloneCar() {
+    const Species = this.constructor[Symbol.species];
+    return new Species(this._brand, this._motor, this._color);
+  }
+
+  get brand() {
+    return this._brand;
+  }
+
+  get motor() {
+    return this._motor;
+  }
+
+  get color() {
+    return this._color;
+  }
+}
