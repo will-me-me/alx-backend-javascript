3fe863cfd521cb40559d3be6837cb4bb28906266
will-me-me <willykaria777@gmail.com>
2024-04-25 02:33:31 -0700

    mplement a class named EVCar that extends the Car class:

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,18 @@
+/* eslint-disable */
+
+import Car from './10-car';
+
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = range;
+  }
+
+  static get [Symbol.species]() {
+    return Car;
+  }
+
+  get range() {
+    return this._range;
+  }
+}
