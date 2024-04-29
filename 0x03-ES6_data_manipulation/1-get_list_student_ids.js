816199dbbb12477bc863d3b4a47b2c133dec27c8
will-me-me <willykaria777@gmail.com>
2024-04-29 04:01:09 -0700

    mapping

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,11 @@
+const getListStudentIds = (listStudents) => {
+  let listStudentIds = [];
+
+  if (!listStudents instanceof Array) {
+    return listStudentIds;
+  }
+  listStudentIds = listStudents.map((student) => student.id);
+  return listStudentIds;
+};
+
+export default getListStudentIds;
